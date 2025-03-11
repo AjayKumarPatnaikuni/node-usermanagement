6c1c5980b74800c940e5f3e6bc2b17395ddcdb50
Manikanta <manikanta.p@appliedsynergy.tech>
2025-03-11 23:00:29 +0530

    port added

--- usermgmt-service/models/userModel.js
+++ usermgmt-service/models/userModel.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: 'localhost:3306',
   user: 'root',
   password: 'password',
   database: 'usermanagement'
